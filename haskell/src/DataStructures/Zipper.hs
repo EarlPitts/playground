ced9d7c568efc56a2514d71beeb98fa90554fd9e
Bendeguz Seres <grafitpor@gmail.com>
2025-07-24 18:27:25 +0200

    Zipper wip

--- haskell/src/DataStructures/Zipper.hs
+++ haskell/src/DataStructures/Zipper.hs
@@ -2,14 +2,30 @@
 
 module DataStructures.Zipper where
 
-step0 = "*  >  *   *  <  **< "
+import Control.Monad
+import Data.Char
+import Data.Foldable
+
+step0 = "*  >  *   *  <  **<"
 
 waveRule :: Char -> Char -> Char -> Char
-waveRule _ l r
+waveRule l _ r
   | fromL && fromR = 'X'
   | fromL = '>'
   | fromR = '<'
   | otherwise = ' '
   where
     fromL = l `elem` "*X>"
-    fromR = l `elem` "*X<"
+    fromR = r `elem` "*X<"
+
+triplets :: [a] -> [[a]]
+triplets cs@(l : m : r : _) = [l, m, r] : triplets (tail cs)
+triplets _ = []
+
+step :: [Char] -> [Char]
+step cs = (\[l,m,r] -> waveRule l m r) <$> triplets (" " <> cs <> " ")
+
+run :: String -> [String]
+run = takeWhile (not . all isSpace) . iterate step
+
+main = traverse_ putStrLn (run step0)
