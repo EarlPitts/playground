ec526b766ce23c72e8fd56ff41af9fdb3f16c3e2
Bendeguz Seres <grafitpor@gmail.com>
2025-08-25 08:05:07 +0200

    Implement selecting pieces

--- miso-experiments/src/Chess.hs
+++ miso-experiments/src/Chess.hs
@@ -6,6 +6,7 @@ import Control.Monad
 import Data.List
 import qualified Data.Set as S
 import Language.Javascript.JSaddle (jsg, (#))
+import Data.Maybe
 import Miso hiding (Off, focus, media_, set, update)
 import Miso.Lens
 import Miso.String (MisoString, ms)
@@ -15,7 +16,7 @@ import Control.Applicative
 -- data Player = White | Black deriving (Show, Eq, MisoString)
 
 data Position = Position Int Int deriving (Show, Eq)
-data Piece = Piece PieceType Position deriving (Show, Eq)
+data Piece = Piece { pieceType :: PieceType, position :: Position } deriving (Show, Eq)
 data PieceType = Pawn | Rook | Knight | Bishop | Queen | King deriving (Show, Eq)
 data Tile = Tile Position (Maybe Piece) deriving (Show, Eq)
 
@@ -23,6 +24,7 @@ data Model = Model
   {
     _blacks :: [Piece]
   , _whites :: [Piece]
+  , _selected :: Maybe Piece
   }
   deriving (Show, Eq)
 
@@ -32,8 +34,11 @@ blacks = lens _blacks $ \m v -> m {_blacks = v}
 whites :: Lens Model [Piece]
 whites = lens _whites $ \m v -> m {_whites = v}
 
+selected :: Lens Model (Maybe Piece)
+selected = lens _selected $ \m v -> m {_selected = v}
+
 initModel :: Model
-initModel = Model bs ws
+initModel = Model bs ws Nothing
   where
     bs = notPawns 0 <> pawns 1
     ws = notPawns 7 <> pawns 6
@@ -57,11 +62,15 @@ chessMain =
     }
 
 data Action
-  = None
+  = Select Piece
+  | Move Piece
   deriving (Show)
 
 updateModel :: Action -> Transition Model Action
-updateModel None = pure ()
+updateModel (Select piece) = do
+  selected .= Just piece
+  m <- use selected
+  io_ $ consoleLog (ms (show m))
 
 positions :: [Position]
 positions = [Position r c | r <- [0 .. 7], c <- [0 .. 7]]
@@ -70,12 +79,12 @@ board :: [Piece] -> Position -> Tile
 board ps pos = Tile pos $ find (\(Piece _ p) -> p == pos) ps
 
 viewModel :: Model -> View Model Action
-viewModel (Model bs ws) =
+viewModel (Model bs ws sel) =
   div_
     [class_ "grid-container"]
     [ div_
         [class_ "container"]
-        (cellView <$> ((board $ bs <> ws) <$> positions))
+        (cellView sel <$> ((board $ bs <> ws) <$> positions))
     ]
 
 isWhite :: Position -> Bool
@@ -83,13 +92,23 @@ isWhite (Position r c) =
   (odd r && odd c) ||
   (even r && even c)
 
-cellView ::Tile -> View Model Action
-cellView (Tile pos Nothing) = if isWhite pos
-  then div_ [class_ "grid-cell-off"] []
-  else div_ [class_ "grid-cell-on"] []
-cellView (Tile pos (Just (Piece t _))) = if isWhite pos
-  then div_ [class_ "grid-cell-off"] [ text (ms (show t)) ]
-  else div_ [class_ "grid-cell-on"] [ text (ms (show t)) ]
+isSelected :: Maybe Piece -> Position -> Bool
+isSelected Nothing _ = False
+isSelected (Just (Piece _ pos)) pos' = pos == pos'
+
+cellView :: Maybe Piece -> Tile -> View Model Action
+cellView sel (Tile pos piece) =
+  div_
+    ([ class_ classStr ] <> clickHandler)
+    (maybe [] (singleton . text . ms . show . pieceType) piece)
+  where
+    classStr = "grid-cell" <> 
+      (if isSelected sel pos then " cell-selected" else "") <>
+      (if (not $ isWhite pos) then " cell-black" else "")
+    clickHandler = case (sel, piece) of
+      (Nothing, Nothing) -> []
+      (Just s, Nothing) -> [ onClick (Move s) ]
+      (_ ,Just p) -> [ onClick (Select p) ]
 
 sheet :: StyleSheet
 sheet =
@@ -109,19 +128,20 @@ sheet =
           gap "0"
         ],
       selector_
-        ".grid-cell-off"
+        ".grid-cell"
         [ border "1px solid #333",
           height "75px",
           width "75px",
           textAlign "center"
         ],
       selector_
-        ".grid-cell-on"
-        [ border "1px solid #333",
-          height "75px",
-          width "75px",
-          background "black",
-          color white,
-          textAlign "center"
+        ".cell-black"
+        [ background "black",
+          color white
+        ],
+      selector_
+        ".cell-selected"
+        [ border "3px solid #f5da42",
+          boxSizing "border-box"
         ]
     ]
