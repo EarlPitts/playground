0e3e7136b0577f85940dd1585156177f5c438f95
Bendeguz Seres <grafitpor@gmail.com>
2025-10-08 07:57:30 +0200

    Add stuff

--- haskell/src/Networking/ChatServer.hs
+++ haskell/src/Networking/ChatServer.hs
@@ -3,6 +3,7 @@
 module Networking.ChatServer where
 
 import Control.Concurrent
+import Control.Concurrent.STM
 import qualified Control.Exception as E
 import Control.Monad
 import qualified Data.ByteString as S
@@ -12,9 +13,10 @@ import qualified Data.List.NonEmpty as NE
 import Debug.Trace
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
+import System.Console.ANSI
 import Prelude hiding (log)
 
-type Server = MVar [Socket] -> Socket -> Int -> IO ()
+type Server = TVar [Socket] -> Socket -> Int -> IO ()
 
 main :: IO ()
 main = runTCPServer Nothing "3000" echo
@@ -23,7 +25,7 @@ debugMode :: Bool
 debugMode = True
 
 log :: String -> IO ()
-log = when debugMode . print
+log = when debugMode . putStrLn . ("[DEBUG] " <>)
 
 mkMsg :: Int -> S.ByteString -> S.ByteString
 mkMsg n msg = "Guest " <> C8.pack (show n) <> ": " <> msg
@@ -38,7 +40,7 @@ echo sockets s n = do
   msg <- recv s 1024
   log $ "message received from socket " <> show s
   unless (S.null msg) $ do
-    socks <- readMVar sockets
+    socks <- readTVarIO sockets
     log $ "got socket list" <> show socks
     sendMessage n msg s socks
     log "sent message to all sockets"
@@ -47,7 +49,7 @@ echo sockets s n = do
 runTCPServer :: Maybe HostName -> ServiceName -> Server -> IO a
 runTCPServer mhost port server = do
   addr <- resolve
-  sockets <- newMVar ([] :: [Socket])
+  sockets <- newTVarIO ([] :: [Socket])
   E.bracket (open addr) close (loop sockets)
   where
     resolve = do
@@ -68,7 +70,30 @@ runTCPServer mhost port server = do
       forever $
         E.bracketOnError (accept sock) (close . fst) $ \(conn, _peer) -> do
           log "accepted connection"
-          modifyMVar_ sockets (\sockets -> pure $ conn : sockets)
-          socketNum <- length <$> readMVar sockets
+          ss <- readTVarIO sockets
+          traverse_ (flip sendAll ("Guest " <> C8.pack (show $ (length ss) + 1) <> " joined\n")) ss
+          socketNum <- atomically $ do
+            ss <- readTVar sockets
+            writeTVar sockets (conn : ss)
+            pure (length ss + 1)
           log "added socket to list"
           void $ forkFinally (server sockets conn socketNum) (const $ gracefulClose conn 5000)
+
+-- Client stuff
+setColor :: Int -> IO ()
+setColor = setSGR . pickColor
+
+pickColor :: Int -> [SGR]
+pickColor n = [foreground i c]
+  where
+    foreground = SetColor Foreground
+    (i, c) = allColors !! mod n 14
+
+allColors :: [(ColorIntensity, Color)]
+allColors = [(i, c) | i <- [Dull, Vivid], c <- cols]
+  where
+    cols = [Red, Green, Yellow, Blue, Magenta, Cyan, White]
+
+resetColor :: IO ()
+resetColor = setSGR [Reset]
+
