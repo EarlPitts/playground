4a762f4675dde9a25749217f1b4f874178136d40
Bendeguz Seres <grafitpor@gmail.com>
2025-10-08 07:10:10 +0200

    Show names in chat

--- haskell/src/Networking/ChatServer.hs
+++ haskell/src/Networking/ChatServer.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Networking.ChatServer where
 
 import Control.Concurrent
 import qualified Control.Exception as E
 import Control.Monad
 import qualified Data.ByteString as S
+import qualified Data.ByteString.Char8 as C8
 import Data.Foldable
 import qualified Data.List.NonEmpty as NE
 import Debug.Trace
@@ -11,28 +14,37 @@ import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
 import Prelude hiding (log)
 
+type Server = MVar [Socket] -> Socket -> Int -> IO ()
+
 main :: IO ()
 main = runTCPServer Nothing "3000" echo
 
 debugMode :: Bool
-debugMode = False
+debugMode = True
 
 log :: String -> IO ()
 log = when debugMode . print
 
-echo :: MVar [Socket] -> Socket -> IO ()
-echo sockets s = do
+mkMsg :: Int -> S.ByteString -> S.ByteString
+mkMsg n msg = "Guest " <> C8.pack (show n) <> ": " <> msg
+
+sendMessage :: Int -> S.ByteString -> Socket -> [Socket] -> IO ()
+sendMessage n msg s socks =
+  traverse_ (flip sendAll (mkMsg n msg)) (filter ((/=) s) socks)
+
+echo :: Server
+echo sockets s n = do
   log "waiting for message..."
   msg <- recv s 1024
   log $ "message received from socket " <> show s
   unless (S.null msg) $ do
     socks <- readMVar sockets
     log $ "got socket list" <> show socks
-    traverse_ (flip sendAll msg) (filter ((/=) s) socks)
+    sendMessage n msg s socks
     log "sent message to all sockets"
-    echo sockets s
+    echo sockets s n
 
-runTCPServer :: Maybe HostName -> ServiceName -> (MVar [Socket] -> Socket -> IO a) -> IO a
+runTCPServer :: Maybe HostName -> ServiceName -> Server -> IO a
 runTCPServer mhost port server = do
   addr <- resolve
   sockets <- newMVar ([] :: [Socket])
@@ -57,5 +69,6 @@ runTCPServer mhost port server = do
         E.bracketOnError (accept sock) (close . fst) $ \(conn, _peer) -> do
           log "accepted connection"
           modifyMVar_ sockets (\sockets -> pure $ conn : sockets)
+          socketNum <- length <$> readMVar sockets
           log "added socket to list"
-          void $ forkFinally (server sockets conn) (const $ gracefulClose conn 5000)
+          void $ forkFinally (server sockets conn socketNum) (const $ gracefulClose conn 5000)
