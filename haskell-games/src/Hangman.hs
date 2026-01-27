b73a0082de1168daf9c113caf66b826270c2f2bd
Bendeguz Seres <grafitpor@gmail.com>
2026-01-27 07:30:36 +0100

    Add gorgeous graphics

--- haskell-games/src/Hangman.hs
+++ haskell-games/src/Hangman.hs
@@ -8,27 +8,22 @@ import qualified Brick.AttrMap as A
 import qualified Brick.Main as M
 import qualified Brick.Types as T
 import Brick.Widgets.Center (center)
-import Brick.Widgets.Table
 import Control.Monad (void)
--- import Network.HTTP.Req
-
 import Control.Monad.IO.Class
-import Data.Aeson (Value (..))
 import Data.Aeson.Decoding
 import Data.Bifunctor
 import Data.List
-import Data.Text (Text, unpack)
+import qualified Data.Set as S
 import qualified Graphics.Vty as V
 import Lens.Micro.Mtl
 import Lens.Micro.TH
 import Network.HTTP.Req
 import System.Random
-import qualified Data.Vector as VEC
-import Data.Aeson.Types (Array)
 
 data AppState = AppState
   { _word :: String,
-    _guessed :: [Char],
+    _guessed :: S.Set Char,
+    _wrong :: S.Set Char,
     _gen :: StdGen
   }
   deriving (Show, Eq)
@@ -38,7 +33,7 @@ makeLenses ''AppState
 getLetters :: String -> [Char]
 getLetters = nub . sort
 
-showGuessed :: String -> [Char] -> String
+showGuessed :: String -> S.Set Char -> String
 showGuessed w ls =
   intersperse ' ' $ (\c -> if c `elem` ls then c else '_') <$> w
 
@@ -47,9 +42,9 @@ initialState w g =
   let letternum = div (length w) 4
       (guessed, _) =
         first
-          (take letternum)
+          (S.fromList . take letternum)
           (uniformShuffleList (getLetters w) g)
-   in AppState w guessed g
+   in AppState w guessed S.empty g
 
 appEvent :: T.BrickEvent () e -> T.EventM () AppState ()
 appEvent (T.VtyEvent e) = case e of
@@ -63,14 +58,56 @@ guess :: Char -> T.EventM () AppState ()
 guess c = do
   w <- use word
   if c `elem` w
-    then guessed %= (c :)
-    else pure ()
+    then guessed %= (S.insert c)
+    else wrong %= (S.insert c)
 
 theMap :: A.AttrMap
 theMap = A.attrMap V.defAttr []
 
 drawUI :: AppState -> [Widget ()]
-drawUI s = [center $ str (showGuessed (s._word) (s._guessed))]
+drawUI s =
+  [ center $
+      drawHanging (s._wrong)
+        <+> ( (str (showGuessed s._word s._guessed))
+                <=> (str $ S.toList s._wrong)
+            )
+  ]
+
+drawHanging :: S.Set Char -> Widget ()
+drawHanging ws
+  | S.size ws <= 2 = str start
+  | S.size ws <= 5 = str half
+  | otherwise = str full
+
+start :: String
+start =
+  "\n\
+  \      _ _ _\n\
+  \      |\n\
+  \      |\n\
+  \      |\n\
+  \      |\n\
+  \     =========\n"
+
+half :: String
+half =
+  "\n\
+  \      _ _ _\n\
+  \      |    O\n\
+  \      |    |\n\
+  \      |\n\
+  \      |\n\
+  \     =========\n"
+
+full :: String
+full =
+  "\n\
+  \      _ _ _\n\
+  \      |    O\n\
+  \      |   /|\\\n\
+  \      |   / \\\n\
+  \      |\n\
+  \     =========\n"
 
 theApp :: M.App AppState e ()
 theApp =
@@ -84,7 +121,7 @@ theApp =
 
 main :: IO ()
 main = do
-  word <- getWord 
+  word <- getWord
   gen <- initStdGen
   void $ M.defaultMain theApp (initialState word gen)
 
