21eab66c9511802a77487c067d8cefb5d3fe74b8
Bendeguz Seres <grafitpor@gmail.com>
2026-02-24 08:14:43 +0100

    Add flake for scala

---
+++ scala/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Scala development environment with sbt and Metals";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            sbt
+            coursier
+          ];
+
+          # Set JAVA_HOME for tools that need it
+          JAVA_HOME = "${pkgs.openjdk}";
+        };
+      }
+    );
+}
