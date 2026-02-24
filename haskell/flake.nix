a49a91f7162a61c0112994fb40f5758ceaad9bad
Bendeguz Seres <grafitpor@gmail.com>
2026-02-24 07:34:13 +0100

    Nix stuff for pinning ghc versions

--- haskell/flake.nix
+++ haskell/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Haskell flake with OpenGL stuff";
+  description = "Haskell flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -20,23 +20,16 @@
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with haskellPackages; [
+          packages = with haskellPackages; [
             ghc
             cabal-install
             haskell-language-server
             hlint
-            pkgs.mesa
-            pkgs.freeglut
-            pkgs.mesa_glu
-            pkgs.zlib
             cabal-fmt
-            # Add any system dependencies here
+            pkgs.zlib
           ];
 
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.freeglut
-            pkgs.mesa
-            pkgs.mesa_glu
             pkgs.zlib
           ];
         };
