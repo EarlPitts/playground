008adbc05beeaa05b3e4c124a74cbb787725b801
Bendeguz Seres <grafitpor@gmail.com>
2026-03-28 10:40:02 +0100

    Add stuff

---
+++ haskell/src/Concurrency/Cache.hs
@@ -0,0 +1,39 @@
+import Control.Concurrent
+import Control.Concurrent.Async
+import Control.Monad
+import Data.Aeson
+import Data.IORef
+import Data.Map
+import Data.Maybe (fromMaybe)
+
+import qualified Data.ByteString as BS
+
+type Catalog = Map String Int
+type Product = String
+type Price = Int
+
+updateCache :: IORef Catalog -> IO ()
+updateCache catalogRef =
+  getCatalog >>= writeIORef catalogRef
+
+getCatalog :: IO Catalog
+getCatalog = do
+  catalogStr <- BS.readFile "catalog.json"
+  throwDecodeStrict catalogStr
+
+priceOrder :: (Product -> Price) -> [Product] -> Price
+priceOrder getPrice products = sum $ getPrice <$> products
+
+doStuff :: IORef Catalog -> IO ()
+doStuff catalogRef = do
+  catalog <- readIORef catalogRef
+  let getPrice p = fromMaybe 0 (Data.Map.lookup p catalog)
+  let totalPrice = priceOrder getPrice ["p1", "p2"]
+  print totalPrice
+
+main :: IO ()
+main = do
+  catalogRef <- getCatalog >>= newIORef
+  concurrently_
+    (forever $ updateCache catalogRef >> threadDelay 5000000)
+    (forever $ doStuff catalogRef >> threadDelay 1000000)
