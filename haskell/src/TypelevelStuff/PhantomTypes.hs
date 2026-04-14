ccb1bcd93f87baec07c142bc2a83279ae861ae87
Bendeguz Seres <grafitpor@gmail.com>
2026-04-14 07:50:05 +0200

    Add stuff

--- haskell/src/TypelevelStuff/PhantomTypes.hs
+++ haskell/src/TypelevelStuff/PhantomTypes.hs
@@ -12,3 +12,30 @@ validate (Email e) = if elem '@' e then Just (Email e) else Nothing
 
 sendEmail :: Email Validated -> IO ()
 sendEmail (Email e) = putStrLn ("Email sent to " ++ e)
+
+-- Only allow checking email for authed user
+data Authenticated
+data Unauthenticated
+
+data User isAuthenticated = User
+  { userName :: String
+  , userEmail :: String
+  , userPassword :: String
+  }
+  deriving (Show, Eq)
+
+getUserName :: User isAuthenticated -> String
+getUserName = userName
+
+getUserEmail :: User Authenticated -> String
+getUserEmail = userEmail
+
+mkUser :: String -> String -> String -> User Unauthenticated
+mkUser = User
+
+authenticate :: User Unauthenticated -> String -> Maybe (User Authenticated)
+authenticate User{..} pass
+  | userPassword == pass = Just User{..}
+  | otherwise = Nothing
+
+user = mkUser "jani" "jani@jani.com" "jani123"
