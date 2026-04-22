c13e32f8719eb713cc3250341031406529905030
Bendeguz Seres <grafitpor@gmail.com>
2026-04-22 07:29:29 +0200

    Add stuff

--- unix-tools/src/HCat.hs
+++ unix-tools/src/HCat.hs
@@ -3,12 +3,15 @@
 
 module HCat where
 
+import Control.Concurrent
 import qualified Control.Exception as Exception
+import Control.Monad (when)
 import qualified Control.Monad.Except as Except
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import System.Console.Terminal.Size
 import System.Environment
+import System.IO (BufferMode (NoBuffering), hSetBuffering, hSetEcho, stdin)
 import System.IO.Error
 
 runHCat :: IO ()
@@ -17,10 +20,12 @@ runHCat = handleIOError $ do
   arg <- handleArgs
   path <- eitherToErr arg
   content <- T.readFile path
-  height <- getHeight <$> size
+  (h, w) <- getDimensions >>= eitherToErr
+
+  let pages = toPages h w content
 
   -- output
-  T.putStr content
+  showPages pages
  where
   handleIOError p =
     Exception.catch p $
@@ -32,14 +37,43 @@ groupsOf n = \case
   as -> case splitAt n as of
     (h, t) -> h : groupsOf n t
 
-getHeight :: Maybe (Window Int) -> Int
-getHeight = \case
-  Just (Window h _) -> h
-  Nothing -> error "baj van"
+newtype Page = Page {getPage :: [T.Text]}
+
+showPages :: [Page] -> IO ()
+showPages = \case
+  [] -> pure ()
+  (p : ps) -> do
+    display p
+    c <- getInput
+    when (c == ' ') $ showPages ps
+
+display :: Page -> IO ()
+display (Page t) = T.putStr $ T.unlines t
+
+toPages :: Int -> Int -> T.Text -> [Page]
+toPages h w content = fmap Page $ groupsOf (h - 1) $ T.lines content
+
+getInput :: IO Char
+getInput = do
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  c <- getChar
+  hSetEcho stdin True
+  if c `elem` [' ', 'q']
+    then pure c
+    else getInput
+
+getDimensions :: IO (Either Error (Int, Int))
+getDimensions = do
+  ds <- size
+  case ds of
+    Just (Window h w) -> pure $ Right (h, w)
+    Nothing -> pure $ Left CannotGetDimensions
 
 data Error
   = NoFileSpecified
   | TooManyFiles
+  | CannotGetDimensions
   deriving (Show)
 
 eitherToErr :: (Show a) => Either a b -> IO b
