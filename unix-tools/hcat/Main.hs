6b66578df379799b1bb92b25cdfae0a2cbd8b6db
Bendeguz Seres <grafitpor@gmail.com>
2026-04-25 08:32:50 +0200

    Add statusline

--- unix-tools/hcat/Main.hs
+++ unix-tools/hcat/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import HCat
-import System.Console.Terminal.Size (Window (..))
+import System.Console.Terminal.Size (Window (..), size)
 
-size :: IO (Maybe (Window Int))
-size = pure (Just (Window 2 2))
+-- size :: IO (Maybe (Window Int))
+-- size = pure (Just (Window 2 2))
 
 main :: IO ()
 main = runHCat size
--- unix-tools/src/HCat.hs
+++ unix-tools/src/HCat.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -7,37 +8,50 @@ module HCat where
 import qualified Control.Exception as Exception
 import Control.Monad (when)
 import qualified Control.Monad.Except as Except
+import qualified Data.ByteString as BS
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Time.Clock as C
+import qualified Data.Time.Clock.POSIX as PC
+import qualified Data.Time.Format as TF
 import System.Console.Terminal.Size (Window (Window))
+import qualified System.Directory as D
 import System.Environment
-import System.IO (BufferMode (NoBuffering), hSetBuffering, hSetEcho, stdin)
+import System.IO (BufferMode (NoBuffering), hSetBuffering, hSetEcho, stdin, stdout)
 import System.IO.Error
+import Text.Printf (printf)
 
 runHCat ::
   IO (Maybe (Window Int)) ->
   IO ()
 runHCat size = handleIOError $ do
-  arg <- handleArgs
-  path <- eitherToErr arg
+  path <- eitherToErr =<< handleArgs
   content <- T.readFile path
-  dimensions <- getDimensions size >>= eitherToErr
+  dimensions <- eitherToErr =<< getDimensions size
+  info <- fileInfo path
+  hSetBuffering stdout NoBuffering
 
-  let pages = paginate dimensions content
+  let pages = paginate dimensions info content
 
-  showPages pages
+  showPages info pages
  where
   handleIOError p =
     Exception.catch p $
       \e -> putStrLn $ "Error: " <> show @IOError e
 
-showPages :: [T.Text] -> IO ()
-showPages = \case
+showPages :: FileInfo -> [T.Text] -> IO ()
+showPages info = \case
   [] -> pure ()
   (p : ps) -> do
+    clearScreen
     T.putStr p
-    input <- getContinue
-    when (input == Continue) (showPages ps)
+    getContinue >>= \case
+      Continue -> showPages info ps
+      Cancel -> pure ()
+
+clearScreen :: IO ()
+clearScreen =
+  BS.putStr "\^[[1J\^[[1;1H"
 
 data ScreenDimensions = ScreenDimensions
   { rows :: Int
@@ -56,13 +70,22 @@ data ContinueCancel
   | Cancel
   deriving (Eq, Show)
 
+data FileInfo = FileInfo
+  { filePath :: FilePath
+  , fileSize :: Int
+  , fileMTime :: C.UTCTime
+  , fileReadable :: Bool
+  , fileWriteable :: Bool
+  , fileExecutable :: Bool
+  }
+  deriving (Show)
+
 -- IO
 getContinue :: IO ContinueCancel
 getContinue = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
   c <- getChar
-  hSetEcho stdin True
   case c of
     ' ' -> pure Continue
     'q' -> pure Cancel
@@ -89,13 +112,32 @@ handleArgs = parseArgs <$> getArgs
     [] -> Left NoFileSpecified
     _ -> Left TooManyFiles
 
+fileInfo :: FilePath -> IO FileInfo
+fileInfo path = do
+  size <- fromIntegral <$> D.getFileSize path
+  mtime <- D.getModificationTime path
+  perm <- D.getPermissions path
+  pure
+    FileInfo
+      { filePath = path
+      , fileSize = size
+      , fileMTime = mtime
+      , fileReadable = D.readable perm
+      , fileWriteable = D.writable perm
+      , fileExecutable = D.executable perm
+      }
+
 -- Core
-paginate :: ScreenDimensions -> T.Text -> [T.Text]
-paginate ScreenDimensions{..} t = T.unlines <$> pageLines
+paginate :: ScreenDimensions -> FileInfo -> T.Text -> [T.Text]
+paginate ScreenDimensions{..} info t = zipWith (<>) pages statusLines
  where
-  unwrappedLines = T.lines t
-  wrappedLines = concatMap (wordWrap cols) unwrappedLines
-  pageLines = groupsOf (rows - 1) wrappedLines
+  rows' = rows - 1
+  wrappedLines = concatMap (wordWrap cols) (T.lines t)
+  pages = T.unlines . padTo rows' <$> groupsOf rows' wrappedLines
+  pageCount = length pages
+  statusLines = formatFileInfo info cols pageCount <$> [1 ..]
+  padTo lineCount rs =
+    take lineCount $ rs <> repeat ""
 
 wordWrap :: Int -> T.Text -> [T.Text]
 wordWrap n t
@@ -109,6 +151,38 @@ wordWrap n t
     | idx <= 0 = (text, T.empty)
     | T.index text idx == ' ' = T.tail <$> T.splitAt idx text
     | otherwise = softWrap text (idx - 1)
+
+formatFileInfo :: FileInfo -> Int -> Int -> Int -> T.Text
+formatFileInfo FileInfo{..} maxWidth totalPages currentPage =
+  invertText (truncateStatus statusline)
+ where
+  invertText inputStr =
+    let
+      reverseVideo = "\^[[7m"
+      resetVideo = "\^[[0m"
+     in
+      reverseVideo <> inputStr <> resetVideo
+  truncateStatus statusLine
+    | maxWidth <= 3 = ""
+    | T.length statusLine > maxWidth =
+        T.take (maxWidth - 3) statusLine <> "..."
+    | otherwise = statusLine
+  permissions =
+    [ if fileReadable then 'r' else '-'
+    , if fileWriteable then 'w' else '-'
+    , if fileExecutable then 'x' else '-'
+    ]
+  timestamp = TF.formatTime TF.defaultTimeLocale "%F %T" fileMTime
+  statusline =
+    T.pack $
+      printf
+        "%s | permissions: %s | %d bytes | modified: %s | page: %d of %d"
+        filePath
+        permissions
+        fileSize
+        timestamp
+        currentPage
+        totalPages
 
 groupsOf :: Int -> [a] -> [[a]]
 groupsOf n = \case
