b68f1d7706b7005d02e9b7ce056fe3e22c44a9fd
Bendeguz Seres <grafitpor@gmail.com>
2026-06-17 06:53:21 +0200

    Make it presentable

--- brainfuck-emulator/app/Emulator.hs
+++ brainfuck-emulator/app/Emulator.hs
@@ -1,113 +1,121 @@
-{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Emulator where
 
-import           Miso
-import           Miso.Html.Element as H
-import           Miso.Html.Event as E
-import           Miso.Html.Property as P
-import           Miso.Types
-import           Miso.Lens
-import Style
 import BrainFuck
 import Data.List.Zipper
 import Data.Word
+import Miso
+import Miso.Html.Element as H
+import Miso.Html.Event as E
+import Miso.Html.Property as P
+import Miso.Lens
+import Miso.Types
+import Style
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as C8
+import qualified Miso.String as T
 
 data Action
-  = AddOne
-  | SubtractOne
-  | SayHelloWorld
+  = Run
+  | Step
+  | Reset
+  | UpdateInput MisoString
   deriving (Show, Eq)
 
-initState = VM {
-  tape = fromList [0]
-  , program = fromList $ parse "<><>>"
-  , output = []
-  , input = []
-}
+initState =
+  VM
+    { tape = fromList (replicate 10 0),
+      program = fromList $ parse "[-]>,[>,]<[.<]",
+      -- input = [72,101,108,108,111,32,87,111,114,108,100,33,0],
+      input = [],
+      output = []
+    }
 
 app :: App VM Action
-app = (component initState updateModel viewModel)
-  { styles = [ Sheet sheet ]
-  }
+app =
+  (component initState updateModel viewModel)
+    { styles = [Sheet sheet]
+    }
 
 updateModel :: Action -> Effect parent props VM Action
-updateModel = undefined
-  -- AddOne ->
-  --   this += 2
-  -- SubtractOne ->
-  --   this -= 1
-  -- SayHelloWorld ->
-  --   io_ (consoleLog "Hello World!")
-
+updateModel = \case
+  Run -> this %= exec'
+  Step -> this %= execStep
+  Reset -> this .= initState
+  UpdateInput i ->
+    this %= (\vm -> vm {input =  BS.unpack (C8.pack (fromMisoString i))})
 
 viewTape :: Zipper Cell -> View VM Action
-viewTape (Zip begin (curr:end)) = H.div_
-    [ P.class_ "counter-display" ]
-    [
-    H.h2_
-      [P.class_ "counter-display"]
-      ["Tape"],
-      text
-        (ms $ show (reverse begin) <> "> " <> show curr <> " <"  <> show end )
-
+viewTape (Zip begin (curr : end)) =
+  H.div_
+    [] $
+    [ H.h2_ [] ["Tape"],
+      H.div_ [P.class_ "zipper-container"] $
+        fmap (viewCell []) (reverse begin)
+        <> [viewCell [P.class_ "zipper-selected"] curr]
+        <> fmap (viewCell []) end
     ]
+
+viewCell :: [Attribute Action] -> Cell -> View VM Action
+viewCell attrs c = H.div_ attrs [text (ms (show c))]
 
 viewProgram :: Zipper Instr -> View VM Action
-viewProgram (Zip begin (curr:end)) = H.div_
-    [ P.class_ "counter-display" ]
-    [
-    H.h2_
-      [P.class_ "counter-display"]
-      ["Tape"],
-      text
-        (ms $ show (reverse begin) <> "> " <> show curr <> " <"  <> show end )
-
+viewProgram (Zip begin (curr : end)) =
+  H.div_
+    []
+    [ H.h2_ [] ["Program"],
+      H.div_ [P.class_ "zipper-container"] $
+        fmap (viewInstr []) (reverse begin)
+        <> [viewInstr [P.class_ "zipper-selected"] curr]
+        <> fmap (viewInstr []) end
+    ]
+viewProgram (Zip begin end) =
+  H.div_
+    []
+    [ H.h2_ [] ["Program"],
+      H.div_ [P.class_ "zipper-container"] $
+        fmap (viewInstr []) (reverse begin)
+        <> fmap (viewInstr []) end
     ]
 
+viewInstr :: [Attribute Action] -> Instr -> View VM Action
+viewInstr attrs c = H.div_ attrs [text (ms (show c))]
+
 viewInput :: [Word8] -> View VM Action
-viewInput ws = H.div_
-    [ P.class_ "counter-display" ]
-    [
-    H.h2_
-      [P.class_ "counter-display"]
-      ["Input"],
-      text (ms $ show ws)
+viewInput ws =
+  H.div_ []
+    [ H.h2_ [] ["Input"],
+      input_ [ type_ "text"
+            , value_ (ms $ C8.unpack (BS.pack ws))
+            , onInput UpdateInput
+            ]
     ]
 
 viewOutput :: [Word8] -> View VM Action
-viewOutput ws = H.div_
-    [ P.class_ "counter-display" ]
-    [
-    H.h2_
-      [P.class_ "counter-display"]
-      ["Output"],
-      text (ms $ show ws)
+viewOutput ws =
+  H.div_ []
+    [ H.h2_ [] ["Output"],
+      text $ ms $ C8.unpack (BS.pack (reverse ws))
     ]
-  
 
 viewModel :: props -> VM -> View VM Action
-viewModel _ VM{..} = H.div_
-  [ P.class_ "counter-container" ]
-  [ H.h1_
-    [ P.class_ "counter-title" ]
-    [ "BF Emulator" ]
-  , viewTape tape
-  , viewProgram program
-  , viewInput input
-  , viewOutput output
-  -- , H.div_
-  --   [ P.class_ "buttons-container"
-  --   ]
-  --   [ H.button_
-  --     [ E.onClick SubtractOne
-  --     , P.class_ "decrement-btn"
-  --     ] [text "-"]
-  --   , H.button_
-  --     [ E.onClick AddOne
-  --     , P.class_ "increment-btn"
-  --     ] [text "+"]
-  --   ]
-  ]
+viewModel _ VM {..} =
+  H.div_ []
+    [ H.h1_ [] ["BF Emulator"]
+    , H.button_
+        [ E.onClick Run
+        ] [text "Run"]
+    , H.button_
+        [ E.onClick Step
+        ] [text "Step"]
+    , H.button_
+        [ E.onClick Reset
+        ] [text "Reset"]
+    , viewTape tape
+    , viewProgram program
+    , viewInput input
+    , viewOutput output
+    ]
--- brainfuck-emulator/app/Style.hs
+++ brainfuck-emulator/app/Style.hs
@@ -29,78 +29,92 @@ sheet =
     , CSS.backgroundColor (CSS.var "background")
     , CSS.color (CSS.var "text-color")
     ]
-  , CSS.selector_ ".counter-container"
-    [ CSS.backgroundColor CSS.white
-    , CSS.padding (CSS.rem 2)
-    , CSS.borderRadius (CSS.px 12)
-    , CSS.boxShadow "shadow"
-    , CSS.textAlign "center"
+  , CSS.selector_ ".zipper-container"
+    [
+      CSS.display "flex"
+    , CSS.flexDirection "row"
+    , CSS.gap "5px"
     ]
-  , CSS.selector_ ".counter-display"
-    [ CSS.fontSize "5rem"
-    , CSS.fontWeight "bold"
-    , CSS.margin "1CSS.rem 0"
-    , CSS.transition "var(--transition)"
+  , CSS.selector_ ".zipper-selected"
+    [
+      CSS.fontWeight "bold"
     ]
-  , CSS.selector_ ".buttons-container"
-    [ CSS.display "flex"
-    , CSS.gap "1rem"
-    , CSS.justifyContent "center"
-    , CSS.marginTop "1.5rem"
-    ]
-  , CSS.selector_ "button"
-    [ CSS.fontSize "1.5rem"
-    , CSS.width "3rem"
-    , CSS.height "3rem"
-    , CSS.border "none"
-    , CSS.borderRadius "50%"
-    , CSS.cursor "pointer"
-    , CSS.transition "var(--transition)"
-    , CSS.color CSS.white
-    , CSS.display "flex"
-    , CSS.alignItems "center"
-    , CSS.justifyContent "center"
-    ]
-  , CSS.selector_ ".increment-btn"
-    [ CSS.backgroundColor (CSS.var "primary-color")
-    ]
-  , CSS.selector_ ".increment-btn:hover"
-    [ CSS.backgroundColor (CSS.var "primary-hover")
-    , CSS.transform "translateY(-2px)"
-    ]
-  , CSS.selector_ ".decrement-btn"
-    [ CSS.backgroundColor (CSS.var "secondary-color")
-    ]
-  , CSS.selector_ ".decrement-btn:hover"
-    [ CSS.backgroundColor (CSS.var "secondary-hover")
-    , CSS.transform "translateY(-2px)"
-    ]
-  , CSS.keyframes_ "pulse"
-    [ CSS.pct 0 =:
-      [ CSS.transform "scale(1)"
-      ]
-    , CSS.pct 50 =:
-      [ CSS.transform "scale(1.1)"
-      ]
-    , CSS.pct 100 =:
-      [ CSS.transform "scale(1)"
-      ]
-    ]
-  , CSS.selector_ ".counter-display.animate"
-    [ CSS.animation "pulse 0.3s ease"
-    ]
-  , CSS.media_ "(max-width: 480px)"
-    [ ".counter-container" =:
-      [ CSS.padding (CSS.rem 1.5)
-      ]
-    , ".counter-display" =:
-      [ CSS.fontSize (CSS.rem 3)
-      ]
-    , "button" =:
-      [ CSS.fontSize (CSS.rem 1.2)
-      , CSS.width (CSS.rem 2.5)
-      , CSS.width (CSS.rem 2.5)
-      ]
-    ]
+  -- , CSS.selector_ ".cell"
+  --   [
+  --     CSS.fontWeight "bold"
+  --   ]
+  -- , CSS.selector_ ".counter-container"
+  --   [ CSS.backgroundColor CSS.white
+  --   , CSS.padding (CSS.rem 2)
+  --   , CSS.borderRadius (CSS.px 12)
+  --   , CSS.boxShadow "shadow"
+  --   , CSS.textAlign "center"
+  --   ]
+  -- , CSS.selector_ ".counter-display"
+  --   [ CSS.fontSize "5rem"
+  --   , CSS.fontWeight "bold"
+  --   , CSS.margin "1CSS.rem 0"
+  --   , CSS.transition "var(--transition)"
+  --   ]
+  -- , CSS.selector_ ".buttons-container"
+  --   [ CSS.display "flex"
+  --   , CSS.gap "1rem"
+  --   , CSS.justifyContent "center"
+  --   , CSS.marginTop "1.5rem"
+  --   ]
+  -- , CSS.selector_ "button"
+  --   [ CSS.fontSize "1.5rem"
+  --   , CSS.width "3rem"
+  --   , CSS.height "3rem"
+  --   , CSS.border "none"
+  --   , CSS.borderRadius "50%"
+  --   , CSS.cursor "pointer"
+  --   , CSS.transition "var(--transition)"
+  --   , CSS.color CSS.white
+  --   , CSS.display "flex"
+  --   , CSS.alignItems "center"
+  --   , CSS.justifyContent "center"
+  --   ]
+  -- , CSS.selector_ ".increment-btn"
+  --   [ CSS.backgroundColor (CSS.var "primary-color")
+  --   ]
+  -- , CSS.selector_ ".increment-btn:hover"
+  --   [ CSS.backgroundColor (CSS.var "primary-hover")
+  --   , CSS.transform "translateY(-2px)"
+  --   ]
+  -- , CSS.selector_ ".decrement-btn"
+  --   [ CSS.backgroundColor (CSS.var "secondary-color")
+  --   ]
+  -- , CSS.selector_ ".decrement-btn:hover"
+  --   [ CSS.backgroundColor (CSS.var "secondary-hover")
+  --   , CSS.transform "translateY(-2px)"
+  --   ]
+  -- , CSS.keyframes_ "pulse"
+  --   [ CSS.pct 0 =:
+  --     [ CSS.transform "scale(1)"
+  --     ]
+  --   , CSS.pct 50 =:
+  --     [ CSS.transform "scale(1.1)"
+  --     ]
+  --   , CSS.pct 100 =:
+  --     [ CSS.transform "scale(1)"
+  --     ]
+  --   ]
+  -- , CSS.selector_ ".counter-display.animate"
+  --   [ CSS.animation "pulse 0.3s ease"
+  --   ]
+  -- , CSS.media_ "(max-width: 480px)"
+  --   [ ".counter-container" =:
+  --     [ CSS.padding (CSS.rem 1.5)
+  --     ]
+  --   , ".counter-display" =:
+  --     [ CSS.fontSize (CSS.rem 3)
+  --     ]
+  --   , "button" =:
+  --     [ CSS.fontSize (CSS.rem 1.2)
+  --     , CSS.width (CSS.rem 2.5)
+  --     , CSS.width (CSS.rem 2.5)
+  --     ]
+  --   ]
   ]
 
