34ca7729a7bd1361f6cf4ad1f253edc8ec236ee3
Bendeguz Seres <grafitpor@gmail.com>
2026-07-17 07:52:51 +0200

    Add stuff

--- chat/src/Client.hs
+++ chat/src/Client.hs
@@ -1,4 +1,66 @@
 module Client where
 
+import Brick
+import qualified Brick.AttrMap as A
+import qualified Brick.Main as M
+import Brick.Widgets.Border
+import Brick.Widgets.Border.Style
+import Brick.Widgets.List as L
+import Data.Vector (Vector (..))
+import qualified Data.Vector as V
+import qualified Graphics.Vty as V
+import Protolude
+
 main :: IO ()
-main = putStrLn "someFunc"
+main = void $ M.defaultMain theApp (AppState dummyMsgs)
+
+dummyMsgs =
+  V.fromList
+    [ Own "hello"
+    , Other "John" "Hey!"
+    , Other "Mike" "Ho!"
+    , Own "Whatsup?"
+    , Other "John" "The sky!"
+    , Own "Haha!"
+    ]
+
+drawUI :: AppState -> [Widget ()]
+drawUI AppState{..} = [history sMessages]
+
+history :: Vector Message -> Widget ()
+history messages =
+  joinBorders
+    $ withBorderStyle unicode
+    $ borderWithLabel (str "Chat")
+    $ padLeftRight 1
+    $ L.renderList
+      ( \_ -> \case
+          (Other name msg) -> txt $ name <> ": " <> msg
+          (Own msg) -> padLeft Max (txt msg)
+      )
+      False
+      (L.list () messages 1)
+
+appEvent _ = undefined
+
+data Message
+  = Other
+      { mUsername :: Text
+      , mText :: Text
+      }
+  | Own {mText :: Text}
+  deriving (Show, Eq)
+
+data AppState = AppState
+  { sMessages :: Vector Message
+  }
+
+theApp :: M.App AppState e ()
+theApp =
+  M.App
+    { M.appDraw = drawUI
+    , M.appChooseCursor = M.showFirstCursor
+    , M.appHandleEvent = appEvent
+    , M.appStartEvent = pure ()
+    , M.appAttrMap = const $ A.attrMap V.defAttr []
+    }
--- chat/src/Server.hs
+++ chat/src/Server.hs
@@ -1,4 +1,33 @@
 module Server where
 
+import Data.IORef
+import Network.Socket
+import Network.Socket.ByteString
+import qualified Data.List.NonEmpty as NE
+import Protolude
+
 main :: IO ()
-main = putStrLn "someFunc"
+main = do
+  socks <- newIORef []
+
+  let hints = defaultHints{addrFlags = [AI_PASSIVE], addrSocketType = Stream}
+  addr <- NE.head <$> getAddrInfo (Just hints) (Just "127.0.0.1") (Just "5000")
+  sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+  bind sock (addrAddress addr)
+  listen sock 1024
+
+  forever $ acceptConns sock socks
+
+acceptConns :: Socket -> IORef [Socket] -> IO ()
+acceptConns sock socksRef = do
+  (conn, _) <- accept sock
+  print "accepted connection"
+  modifyIORef socksRef (conn :)
+  void $ async $ handleUser conn socksRef
+
+handleUser :: Socket -> IORef [Socket] -> IO ()
+handleUser sock socksRef = do
+  msg <- recv sock 1024
+  socks <- readIORef socksRef
+  traverse_ (flip send $ msg) socks
+  handleUser sock socksRef
