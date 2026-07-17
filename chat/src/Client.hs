0f49951431d775b43bf0bc4c2c459587acccefba
Bendeguz Seres <bendeguz.seres@emarsys.com>
2026-07-17 16:05:52 +0200

    WIP

--- chat/src/Client.hs
+++ chat/src/Client.hs
@@ -2,6 +2,7 @@ module Client where
 
 import Brick
 import qualified Brick.AttrMap as A
+import Brick.BChan
 import qualified Brick.Main as M
 import qualified Brick.Types as T
 import Brick.Widgets.Border
@@ -11,11 +12,17 @@ import Brick.Widgets.List as L
 import Data.List (head)
 import Data.Vector (Vector (..))
 import qualified Data.Vector as V
+import Graphics.Vty (defaultConfig)
 import qualified Graphics.Vty as V
+import Graphics.Vty.Platform.Unix (mkVty)
 import Protolude hiding (head)
 
 main :: IO ()
-main = void $ M.defaultMain theApp (AppState dummyMsgs emptyEditor)
+main = do
+  chan <- newBChan 10
+  let buildVty = mkVty defaultConfig
+  initialVty <- buildVty
+  void $ M.customMain initialVty buildVty (Just chan) theApp (AppState emptyHistory emptyEditor)
 
 dummyMsgs =
   V.fromList
@@ -28,11 +35,14 @@ dummyMsgs =
     ]
 
 emptyEditor = editor 2 (Just 1) ""
+emptyHistory = L.list 1 dummyMsgs 1
 
 drawUI :: AppState -> [Widget Int]
-drawUI AppState{..} = [vBox [history sMessages, textBox sEditor]]
+drawUI AppState{..} = [vBox [history sHistory, textBox sEditor]]
 
-history :: Vector Message -> Widget Int
+data Event = NewMessage Text
+
+history :: List Int Message -> Widget Int
 history messages =
   joinBorders $
     withBorderStyle unicode $
@@ -44,24 +54,26 @@ history messages =
                 (Own msg) -> padLeft Max (txt msg)
             )
             False
-            (L.list 1 messages 1)
+            messages
 
 textBox :: Editor Text Int -> Widget Int
 textBox =
-  borderWithLabel (str "Reply")
+  borderWithLabel (txt "Reply")
     . renderEditor (\t -> txt $ head t) True
 
-appEvent :: BrickEvent Int e -> EventM Int AppState ()
+appEvent :: BrickEvent Int Text -> EventM Int AppState ()
 appEvent (T.VtyEvent e) = case e of
   V.EvKey (V.KEsc) [] -> M.halt
   V.EvKey (V.KEnter) [] -> do
     editorState <- gets sEditor
     let msg = head $ getEditContents editorState
-    modify (\s -> s{sEditor = emptyEditor})
+    modify (\s -> s{sHistory = L.list 1 (V.snoc (listElements $ sHistory s) (Own msg)) 1, sEditor = emptyEditor})
   _ -> do
     editorState <- gets sEditor
     newEditorState <- nestEventM' editorState $ handleEditorEvent (VtyEvent e)
     modify (\s -> s{sEditor = newEditorState})
+appEvent (T.AppEvent msg) =
+  modify (\s -> s{sHistory = L.list 1 (V.snoc (listElements $ sHistory s) (Other "jani" msg)) 1}) -- TODO
 appEvent _ = pure ()
 
 data Message
@@ -73,11 +85,11 @@ data Message
   deriving (Show, Eq)
 
 data AppState = AppState
-  { sMessages :: Vector Message
+  { sHistory :: List Int Message
   , sEditor :: Editor Text Int
   }
 
-theApp :: M.App AppState e Int
+theApp :: M.App AppState Text Int
 theApp =
   M.App
     { M.appDraw = drawUI
