2c533b8e0a49baa6e992d0cd2d2438bcaa5b0673
Bendeguz Seres <grafitpor@gmail.com>
2026-07-30 07:07:16 +0200

    hlox initial commit

---
+++ hlox/app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import qualified Hlox (main)
+
+main :: IO ()
+main = Hlox.main
---
+++ hlox/src/Hlox.hs
@@ -0,0 +1,34 @@
+module Hlox where
+
+import Control.Monad
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import System.Environment
+import System.Exit
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [] -> runRepl
+    [fileName] -> runScript fileName
+    _otherwise -> do
+      putStrLn "Usage: hlox [script]"
+      exitWith (ExitFailure 64)
+
+runRepl :: IO ()
+runRepl = do
+  putStr "> "
+  line <- TIO.getLine
+  unless (T.null line) $ do
+    run line
+    runRepl
+
+runScript :: FilePath -> IO ()
+runScript path = do
+  script <- TIO.readFile path
+  run script
+
+run :: Text -> IO ()
+run _ = pure ()
