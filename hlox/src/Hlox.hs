203f71c31a8899e15444b904c345adc2f27b6418
Bendeguz Seres <grafitpor@gmail.com>
2026-08-05 07:48:34 +0200

    Start evaluator

--- hlox/src/Hlox.hs
+++ hlox/src/Hlox.hs
@@ -3,13 +3,15 @@
 module Hlox (main) where
 
 import Control.Monad
+import Control.Monad.State
+import Data.Map
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.Environment
 import System.Exit
 import System.IO (hFlush, stdout)
-import Text.Parsec
+import Text.Parsec hiding (State)
 
 import Parser
 
@@ -39,5 +41,29 @@ runScript path = do
 
 run :: Text -> IO ()
 run script = case parse p "" script of
-  Right ast -> print ast
+  Right ast -> print (evalState (exec ast) mempty)
   Left err -> print err
+
+data Value
+  = NilValue
+  | NumValue Double
+  | StringValue String
+  | BoolValue Bool
+  deriving (Eq, Show)
+
+data TypeError = TypeError deriving (Show)
+
+exec :: [Statement] -> State (Map String Value) (Either TypeError Value)
+exec [ExprStatement e] = pure $ eval e
+
+eval :: Expr -> Either TypeError Value
+eval (BoolLit b) = Right (BoolValue b)
+eval (StringLit s) = Right (StringValue s)
+eval (NumLit n) = Right (NumValue n)
+eval Nil = Right NilValue
+eval (Unary Neg e) = case eval e of
+  Right (BoolValue b) -> Right (BoolValue (not b))
+  _ -> Left TypeError
+eval (Unary Minus e) = case eval e of
+  Right (NumValue n) -> Right (NumValue (-n))
+  _ -> Left TypeError
--- hlox/src/Parser.hs
+++ hlox/src/Parser.hs
@@ -1,6 +1,8 @@
 module Parser (
   p,
   Expr (..),
+  Op (..),
+  Statement (..),
 ) where
 
 import Text.Parsec
