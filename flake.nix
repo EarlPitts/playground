dd1041f523e10792596492c30261c31c9362e444
Bendeguz Seres <grafitpor@gmail.com>
2026-09-25 09:21:17 +0200

    Add opengl stuff to the main flake

--- flake.nix
+++ flake.nix
@@ -17,33 +17,46 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         haskell = pkgs.haskellPackages;
+        toolchain = with pkgs; [
+          # Haskell
+          haskell.ghc
+          haskell.cabal-install
+          haskell.haskell-language-server
+          haskell.hlint
+          haskell.cabal-fmt
+
+          # Scala
+          openjdk
+          sbt
+          coursier
+
+          # Misc
+          sqlite
+        ];
+
       in
       {
         packages.default = pkgs.haskell.lib.justStaticExecutables (
           haskell.callCabal2nix "imageboard" ./imageboard { }
         );
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            # Haskell
-            haskell.ghc
-            haskell.cabal-install
-            haskell.haskell-language-server
-            haskell.hlint
-            haskell.cabal-fmt
+        devShells = {
+          default = pkgs.mkShell {
+            packages = toolchain;
+            buildInputs = [
+              pkgs.zlib
+            ];
+          };
 
-            # Scala
-            openjdk
-            sbt
-            coursier
-
-            # Misc
-            sqlite
-          ];
-
-          buildInputs = [
-            pkgs.zlib
-          ];
+          opengl = pkgs.mkShell {
+            packages = toolchain;
+            buildInputs = [
+              pkgs.zlib
+              pkgs.freeglut
+              pkgs.mesa
+              pkgs.mesa_glu
+            ];
+          };
         };
       }
     );
